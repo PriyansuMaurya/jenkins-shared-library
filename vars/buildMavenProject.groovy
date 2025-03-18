4cf7dee66cc921b17ad85adc829392ddfefa38bf
Priyanshu Maurya <priyanshu1999pm@gmail.com>
2025-03-18 15:18:39 +0000

    Added shared library for Maven builds

--- vars/buildMavenProject.groovy
+++ vars/buildMavenProject.groovy
@@ -1,23 +1,18 @@
 def call(String projectPath = '.') {
-    pipeline {
-        agent any
-        stages {
-            stage('Build Maven Project') {
-                steps {
-                    script {
-                        try {
-                            echo "Building Maven Project in: ${projectPath}"
-                            def buildResult = sh(script: "cd ${projectPath} && mvn clean package", returnStatus: true)
+    stage('Build Maven Project') {
+        steps {
+            script {
+                try {
+                    echo "Building Maven Project in: ${projectPath}"
+                    def buildResult = sh(script: "cd ${projectPath} && mvn clean package", returnStatus: true)
 
-                            if (buildResult == 0) {
-                                echo "✅ Build was successful!"
-                            } else {
-                                error "❌ Build failed. Check logs for details."
-                            }
-                        } catch (Exception e) {
-                            error "Build process encountered an error: ${e.message}"
-                        }
+                    if (buildResult == 0) {
+                        echo "✅ Build was successful!"
+                    } else {
+                        error "❌ Build failed. Check logs for details."
                     }
+                } catch (Exception e) {
+                    error "Build process encountered an error: ${e.message}"
                 }
             }
         }
